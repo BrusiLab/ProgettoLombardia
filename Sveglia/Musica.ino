a6ad363723a013779ab7e5e62728ca3db4c61a37
Ongaretti <stefano.ongaretti@gmail.com>
2024-08-27 17:31:34 +0200

    Riorganizzazione sveglia
    Aggiunto nuovo tab con note e melodie
    Aggiunto pulsante emergenza

---
+++ Sveglia/Musica.ino
@@ -0,0 +1,131 @@
+#define frq 1000
+#define durata 250
+
+#define scalaalta
+
+#ifdef scalaalta
+
+#define Do 523
+#define re 587
+#define mi 659
+#define fa 699
+#define sol 784
+#define la 880
+#define si 988
+#define DO 1047
+#define RE 1175
+#define MI 1319
+
+#endif
+
+#ifdef scalabassa
+
+#define Do 262
+#define re 293
+#define mi 330
+#define fa 349
+#define sol 392
+#define la 440
+#define si 494
+#define DO 523
+#define RE 587
+#define MI 659
+
+#endif
+
+void suona(int nota, float tempo) {
+  tone(buzzer, nota, durata * tempo);
+  delay(durata * 1.3 * tempo);
+}
+
+void sveglia() {
+
+  while (fermati != true && orario == true) {
+
+    digitalWrite(led, HIGH);
+
+    suona(MI, 0.5);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(RE, 0.5);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(fa, 1);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(sol, 1);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(DO, 0.5);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(si, 0.5);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(re, 1);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(mi, 1);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(si, 0.5);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(la, 0.5);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(Do, 1);
+    stop();
+    digitalWrite(led, LOW);
+
+    suona(mi, 1);
+    stop();
+    digitalWrite(led, HIGH);
+
+    suona(la, 3);
+    stop();
+    digitalWrite(led, LOW);
+
+    stop();
+  }
+
+  orario = false;
+  fermati = false;
+}
+
+void allarme() {
+  
+  while (fermati != true && pericolo == true) {
+
+    tone(buzzer, frq, 500);
+    stop();
+    tone(buzzer, frq, 500);
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    stop();
+    digitalWrite(led, LOW);
+    delay(200);
+    stop();
+
+    digitalWrite(led, HIGH);
+    delay(200);
+    stop();
+    digitalWrite(led, LOW);
+    delay(200);
+    stop();
+  }
+
+  pericolo = false;
+  fermati = false;
+}
--- Sveglia/Sveglia.ino
+++ Sveglia/Sveglia.ino
@@ -1,58 +1,23 @@
-//Hardware: collego - a gnd e S a pin digitale con pwm
+//Hardware buzzer: collego - a gnd e S a pin digitale con pwm
 
-#define pin 10
+#define disinnesco 8  //disattiva sveglia e allarme
+#define emergenza 9   //attiva allarme
+#define buzzer 10
 #define led 11
-#define disinnesco 8
 
-#define frq 1000
-#define durata 250
-
-#define scalaalta
-
-#ifdef scalaalta
-
-#define Do 523
-#define re 587
-#define mi 659
-#define fa 699
-#define sol 784
-#define la 880
-#define si 988
-#define DO 1047
-#define RE 1175
-#define MI 1319
-
-#endif
-
-#ifdef scalabassa
-
-#define Do 262
-#define re 293
-#define mi 330
-#define fa 349
-#define sol 392
-#define la 440
-#define si 494
-#define DO 523
-#define RE 587
-#define MI 659
-
-#endif
-
-bool orario = true;
-bool pericolo;
-bool fermati;
+bool orario;    //attivato a ora sveglia
+bool pericolo;  //attivato da puslante emergenza o caduta
+bool fermati;   //attivato da disinnesco -> ferma sveglia e allarme
 
 void setup() {
+
   Serial.begin(9600);
-  pinMode(pin, OUTPUT);
+  
+  pinMode(buzzer, OUTPUT);
   pinMode(led, OUTPUT);
   pinMode(disinnesco, INPUT);
-}
+  pinMode(emergenza, INPUT);
 
-void suona(int nota, float tempo) {
-  tone(pin, nota, durata * tempo);
-  delay(durata * 1.3 * tempo);
 }
 
 void stop() {
@@ -61,96 +26,13 @@ void stop() {
   }
 }
 
-void sveglia() {
-
-  digitalWrite(led, HIGH);
-
-  suona(MI, 0.5);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(RE, 0.5);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(fa, 1);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(sol, 1);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(DO, 0.5);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(si, 0.5);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(re, 1);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(mi, 1);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(si, 0.5);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(la, 0.5);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(Do, 1);
-  stop();
-  digitalWrite(led, LOW);
-
-  suona(mi, 1);
-  stop();
-  digitalWrite(led, HIGH);
-
-  suona(la, 3);
-  stop();
-  digitalWrite(led, LOW);
-}
-
 void loop() {
 
-  stop();
-
-  while (fermati != true && orario == true) {
-    sveglia();
-    stop();
+  if (digitalRead(emergenza) == true){
+    pericolo = true;
   }
 
-  orario = false;
-  fermati = false;
+  sveglia();
+  allarme();
 
-  while (fermati != true && pericolo == true) {
-
-    tone(pin, frq, 500);
-    stop();
-    tone(pin, frq, 500);
-
-    digitalWrite(led, HIGH);
-    delay(200);
-    stop();
-    digitalWrite(led, LOW);
-    delay(200);
-    stop();
-
-    digitalWrite(led, HIGH);
-    delay(200);
-    stop();
-    digitalWrite(led, LOW);
-    delay(200);
-    stop();
-  }
-
-  pericolo = false;
-  fermati = false;
 }
