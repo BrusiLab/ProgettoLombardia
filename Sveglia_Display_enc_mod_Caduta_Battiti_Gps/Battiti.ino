8d1bbdf78b458dabcba063b5bb9f20f98e547934
Ongaretti <stefano.ongaretti@gmail.com>
2024-09-12 14:00:56 +0200

    update battitri

--- Sveglia_Display_enc_mod_Caduta_Battiti_Gps/Battiti.ino
+++ Sveglia_Display_enc_mod_Caduta_Battiti_Gps/Battiti.ino
@@ -22,23 +22,16 @@ int misurabattiti;
 
 void inizializzaBattiti() {  //inizializzazione del sensore
   // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST))  //Use default I2C port, 400kHz speed
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println(F("MAX30105 was not found. Please check wiring/power."));  //messaggio di errore e blocco al codice se inizializzazioen fallisce
-    while (1)
-      ;
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
   }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-
-  byte ledBrightness = 60;  //Options: 0=Off to 255=50mA
-  byte sampleAverage = 4;   //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2;         //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100;    //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 411;     //Options: 69, 118, 215, 411
-  int adcRange = 4096;      //Options: 2048, 4096, 8192, 16384
-
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);  //Configure sensor with these settings
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
 int iniziorilevazione;
